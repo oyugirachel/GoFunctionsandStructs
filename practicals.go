578a71a1042db90e0dd36a5754a6e1559d356600
oyugirachel <oyugirachel20@gmail.com>
2020-10-02 09:44:28 +0300

    appending to structs

--- practicals.go
+++ practicals.go
@@ -6,9 +6,27 @@ type Bootcamp struct {
 	Lat float64
 	Lon float64
 }
+type User struct {
+	Id       int
+	Name     string
+	Location string
+}
+
+type Player struct {
+	Id       int
+	Name     string
+	Location string
+	GameId	 int
+}
 func main() {
 	x := new(Bootcamp)
 	y := &Bootcamp{}
 	fmt.Println(*x == *y)
+	p := Player{}
+	p.Id = 42
+	p.Name = "Matt"
+	p.Location = "LA"
+	p.GameId = 90404
+	fmt.Printf("%+v", p)
 }
 
