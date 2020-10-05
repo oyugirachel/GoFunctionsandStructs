c6fc1f15bbea07195dfcbdc6238256c36cdb1516
oyugirachel <oyugirachel20@gmail.com>
2020-10-05 11:48:00 +0300

    dealt with multidimensional arrays

--- slices.go
+++ slices.go
@@ -17,4 +17,13 @@ func main() {
 	// [hello world!]
 	fmt.Printf("%q\n", c)
 	// ["hello" "world!"]
+	var d [2][3]string
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			d[i][j] = fmt.Sprintf("row %d - column %d", i+1, j+1)
+		}
+	}
+	fmt.Printf("%q", a)
+	// [["row 1 - column 1" "row 1 - column 2" "row 1 - column 3"]
+	//  ["row 2 - column 1" "row 2 - column 2" "row 2 - column 3"]]
 }
